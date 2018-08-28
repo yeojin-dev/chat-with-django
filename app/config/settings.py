c2e3957f607e16f41fc32d25c0bb4de8dd5ed266
Yeojin Kim <yeojin.dev@gmail.com>
2018-08-28 10:45:10 +0900

    ADD: ASGI_APPLICATION 설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'chat',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ASGI_APPLICATION = 'config.routing.application'

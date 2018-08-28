09f8709b3b979f33a36a38c918dcd2abf911c63e
Yeojin Kim <yeojin.dev@gmail.com>
2018-08-28 12:40:09 +0900

    ENH: Enable a channel layer

--- app/config/settings.py
+++ app/config/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ASGI_APPLICATION = 'config.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
